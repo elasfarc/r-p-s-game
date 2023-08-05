500740590ccae5f8d3e4cf21e74c5be7c9fd6916
Nahuel Alexis Balsas Faravelli <122588119+Nahuel149@users.noreply.github.com>
2023-08-05 18:33:29 +0900

    Add files via upload

--- src/game.js
+++ src/game.js
@@ -1,117 +1,120 @@
-import { ACTIONS, dispatch, getState } from "./g_state.js";
-import { capitalize, sleep, getRandomInt } from "./helpers.js";
-import { SELECTIONS, SYMBOLS, MODES, WIN_CASE } from "./game_const.js";
-
-function getGame() {
-  return { init, start };
-  /**/
-  async function init() {
-    const modeInput = +prompt(
-      `choose the game mode \n 0: ${MODES[0]} \n 1: ${MODES[1]}"`,
-      "0",
-    );
-
-    const { isSingle, mode } = dispatch({
-      action:
-        modeInput === 0
-          ? ACTIONS.CHANGE_MODE_SINGLE
-          : ACTIONS.CHANGE_MODE_MULTI,
-    });
-
-    console.log(`=========\nMODE: ${mode} \n=========\n
-        Okay master${!isSingle ? "s" : ""}, Let's start with your name${
-          !isSingle ? "s" : ""
-        }\n
-        `);
-    await sleep(1000);
-    const p1Name = prompt("What's your name master?");
-    dispatch({ action: ACTIONS.CHANGE_NAME_P1, data: p1Name });
-    console.log(`Okay master ${p1Name}!`);
-    if (!isSingle)
-      dispatch({
-        action: ACTIONS.CHANGE_NAME_P2,
-        data: prompt("What's about the other master?"),
-      });
-    await sleep(1000);
-    console.log(
-      `=========\nLet's get Started \n=========\n Rock, Paper, Scissors SHOOOOT `,
-    );
-  }
-
-  async function start() {
-    const { roundCount, currentPlayer, isSingle, names } = getState();
-    let i = 0;
-    while (i < roundCount) {
-      const p1Selection = playerSelection(names[currentPlayer]);
-      const p2Selection = isSingle
-        ? computerPlay()
-        : playerSelection(names[currentPlayer]);
-      const { winner, msg } = playRound({
-        names,
-        selections: { p1Selection, p2Selection },
-        round: i,
-      });
-
-      dispatch({
-        action: ACTIONS.MAKE_MOVE,
-        data: { winner, round: i, move: { p1: p1Selection, p2: p2Selection } },
-      });
-
-      i++;
-
-      console.log(`
-      ==============================
-                ${msg}
-      ==============================
-      `);
-    }
-  }
-}
-
-function playRound({ names, selections: { p1Selection, p2Selection }, round }) {
-  return isDraw(p1Selection, p2Selection)
-    ? { winner: null, msg: `Round:${round}\n It's a Draw, Let's go again!` }
-    : isP1Win(p1Selection, p2Selection)
-    ? {
-        winner: "p1",
-        msg: `Round:${round + 1}\n ${names.p1} won, ${p1Selection} ${
-          SYMBOLS[p1Selection]
-        } beats ${p2Selection} ${SYMBOLS[p2Selection]}!`,
-      }
-    : {
-        winner: "p2",
-        msg: `Round:${round + 1}\n ${names.p2} won, ${p2Selection} ${
-          SYMBOLS[p2Selection]
-        } beats ${p1Selection} ${SYMBOLS[p1Selection]}!`,
-      };
-}
-
-function isP1Win(p1, p2) {
-  return WIN_CASE[p1] === p2;
-}
-
-function isDraw(p1, p2) {
-  return p1 === p2;
-}
+// Function to generate a randomly returns Rock, paper or scissors.
 function computerPlay() {
-  const randomNum = getRandomInt({
-    from: 0,
-    to: 3,
-  });
-
-  return randomNum === 0
-    ? SELECTIONS[0]
-    : randomNum === 1
-    ? SELECTIONS[1]
-    : SELECTIONS[2];
+    const plays = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random() * plays.length);
+    return plays[randomIndex];
 }
 
-function playerSelection(playerName) {
-  const usrInput = prompt(
-    `Yo ${playerName}! Rock, Paper or Scissors?!`,
-    "Rock",
-  );
-  return capitalize(usrInput);
+function playRound(playerSelection, computerSelection) {
+    // Make playerSelection case-insensitive
+    playerSelection = playerSelection.toLowerCase();
+
+    // Capitalize the first letter of the player's selection
+    const formattedPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+
+    // Check for a tie
+    if (formattedPlayerSelection === computerSelection) {
+        return "It's a draw!";
+    } else if (
+        (formattedPlayerSelection === "Rock" && computerSelection === "Scissors") ||
+        (formattedPlayerSelection === "Paper" && computerSelection === "Rock") ||
+        (formattedPlayerSelection === "Scissors" && computerSelection === "Paper")
+    ) {
+        return "You won this round! " + formattedPlayerSelection + " beats " + computerSelection;
+    } else {
+        return "You lose this round! " + computerSelection + " beats " + formattedPlayerSelection;
+    }
 }
 
-export { getGame };
+function game() {
+    let playAgain = true;
+
+    while (playAgain) {
+        let playerScore = 0;
+        let computerScore = 0;
+
+        for (let round = 1; round <= 5; round++) {
+            let validSelection = false;
+            let playerSelection;
+
+            while (!validSelection) {
+                playerSelection = prompt('Round ' + round + ': Choose Rock, Paper, or Scissors');
+
+                if (["rock", "paper", "scissors"].includes(playerSelection.toLowerCase())) {
+                    validSelection = true;
+                } else {
+                    console.log("Be careful with your fat fingers! Please choose Rock, Paper, or Scissors.");
+                }
+            }
+
+            const computerSelection = computerPlay();
+
+            console.log('Computer plays: ' + computerSelection);
+            const result = playRound(playerSelection, computerSelection);
+            console.log(result);
+
+            if (result.includes('won')) {
+                playerScore++;
+            } else if (result.includes('lose')) {
+                computerScore++;
+            }
+        }
+
+        // Report the final scores and determine the winner
+        console.log('Final Scores:');
+        console.log('Player: ' + playerScore);
+        console.log('Computer: ' + computerScore);
+
+        if (playerScore > computerScore) {
+            console.log("You are unstoppable! You won this game!");
+        } else if (playerScore < computerScore) {
+            console.log("You are a loser! Try to play something else!");
+        } else {
+            console.log("It's a draw. Play it again to prove you're good!");
+        }
+
+        // Determine the winner and set the appropriate prompt message
+        let promptMessage;
+        if (playerScore > computerScore) {
+            promptMessage = "Congrats! You are so good, but you think you can win again? (yes or no)";
+        } else {
+            promptMessage = "Do you have the courage to play again, chicken? (yes or no)";
+        }
+
+        // Ask the player if they want to play again, ensuring a valid response
+        let validResponse = false;
+        while (!validResponse) {
+            const playAgainResponse = prompt(promptMessage);
+            if (playAgainResponse.toLowerCase() === "yes" || playAgainResponse.toLowerCase() === "no") {
+                playAgain = playAgainResponse.toLowerCase() === "yes";
+                validResponse = true;
+            } else {
+                console.log("Again fat fingers? Please answer with 'yes' or 'no'.");
+            }
+        }
+
+        // Clear the terminal (console) before starting a new game
+        console.clear();
+
+        if (!playAgain) {
+            let validConfirmResponse = false;
+            while (!validConfirmResponse) {
+                const confirmResponse = prompt("So you are a chicken? (yes or no)");
+                if (confirmResponse.toLowerCase() === "yes" || confirmResponse.toLowerCase() === "no") {
+                    validConfirmResponse = true;
+                    if (confirmResponse.toLowerCase() === "yes") {
+                        console.log("Oh I see, so you are a real chicken then, give this game away to a real gamer!");
+                    } else {
+                        // If player is not sure, continue the game loop
+                        playAgain = true;
+                    }
+                } else {
+                    console.log("Again fat fingers? Please answer with 'yes' or 'no'.");
+                }
+            }
+        }
+    }
+}
+
+// Call the game function to start the game loop
+game();
